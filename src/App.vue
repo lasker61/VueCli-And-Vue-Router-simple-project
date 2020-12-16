5ce70703eedbf26b41b9a5355c7d2dc7ca4483e6
lasker61 <laskernahid2@gmail.com>
2020-12-16 23:00:49 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>|
+      <router-link to="/contact">Contact</router-link>
+
+    <router-view/>
+  </div>
+</template>
+
